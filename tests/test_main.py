96d615db4b3e227720234b413a0146a7bbc9403d
wklein1 <Witali@>
2022-10-23 01:06:31 +0200

    #1 add get components endpoint test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,20 @@
-def test_main():
-    pass
+from fastapi.testclient import TestClient
+from main import app
+
+def test_get_components_endpoint_returns_components():
+    client = TestClient(app)
+    response = client.get("/components")
+    assert response.status_code == 200
+    assert response.json()[0] == {
+        "id": 0,
+        "name": "EVGA SuperNOVA GT 850 850W",
+        "vendor": "notebooksbilliger.de",
+        "price": 89.90,
+        "description": "",
+        "location": "Germany",
+        "manufacturer": "220-GT-0850-Y2",
+        "productGroup": "Power-supply",
+        "weight": 300.0,
+        "status": "new",
+        "eanNumber": "4250812439109"
+  }
