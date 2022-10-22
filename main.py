05feb2f8fe39f79ed4c76681f7e26d3104203e56
wklein1 <Witali@>
2022-10-23 01:41:14 +0200

    #1 add component model, add get components endpoint

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from models.component_model import Component
+from decouple import config
+import requests
+
+COMPONENTS_SERVICE_API_KEY = config("COMPONENTS_SERVICE_API_KEY")
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"])
+
+@app.get("/components", response_model=list[Component])
+async def get_components():
+    headers = {'Content-Type': 'application/json','X-API-Key':COMPONENTS_SERVICE_API_KEY}
+    response = requests.get("https://components-service.deta.dev/components", headers=headers)
+    return response.json()
