22c773a40b45d94ef62dc183d32182202720619c
wklein1 <Witali@>
2022-11-02 16:10:40 +0100

    #24 add get currencies endpoint tests

---
+++ tests/test_currency_service_routes.py
@@ -0,0 +1,38 @@
+from fastapi.testclient import TestClient
+from main import app
+
+def test_get_currencies_endpoint():
+    #ARRANGE
+    client = TestClient(app)
+    
+    expected_currencies_sublist = [
+        {
+            "code":"EUR",
+            "symbol":"€",
+            "name":"Euro",
+            "country":"European Union"
+        }, 
+        {
+            "code":"COP",
+            "symbol":"$",
+            "name":"Colombian peso",
+            "country":"Colombia"
+        }
+    ]
+    #ACT
+    response = client.get("/currencies")
+    #ASSERT
+    assert response.status_code == 200
+    assert all(currencie in response.json() for currencie in expected_currencies_sublist)
+
+
+def test_get_currency_exchange_rate():
+    #ARRANGE
+    client = TestClient(app)
+    
+    expected_key = "exchangeRate"
+    #ACT
+    response = client.get("/currencies/EUR/USD")
+    #ASSERT
+    assert response.status_code == 200
+    assert response.json()[expected_key]
