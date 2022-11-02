a3d3d5e487f4c3d1e1b944d383278e802b26f7fe
wklein1 <Witali@>
2022-11-02 16:31:17 +0100

    #24 add get currencies endpoint

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 from models.component_model import Component
+from models import error_models, currency_models
 from decouple import config
 import requests
 
@@ -30,4 +31,25 @@ app.add_middleware(
 async def get_components():
     headers = {'Content-Type': 'application/json','X-API-Key':COMPONENTS_SERVICE_API_KEY}
     response = requests.get("https://cs-components-service.deta.dev/components", headers=headers)
+    return response.json()
+
+
+@app.get(
+    "/currencies",
+    response_model=list[currency_models.CurrencyModel],
+    response_description="Returns list of available currencies",
+    responses={503 :{
+            "model": error_models.HTTPErrorModel,
+            "description": "Error raised if microservice request fails."
+        }},
+    description="Get all available currencies.",   
+    tags=["currency microservice"] 
+)
+async def get_currencies():
+    headers = {'Content-Type': 'application/json','X-API-Key':COMPONENTS_SERVICE_API_KEY}
+    response = requests.get("https://cs-currency-service.deta.dev/currencies", headers=headers)
+    if response.status_code == status.HTTP_503_SERVICE_UNAVAILABLE:
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE)
+    if response.json() == {}:
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE)
     return response.json()
