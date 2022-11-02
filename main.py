3cc65b818c736be281bd7977813f5a71a1c39a9b
wklein1 <Witali@>
2022-11-02 20:21:59 +0100

    #12 adjust microservice access token lifetime

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ async def delete_user(passwordIn:auth_models.PasswordInModel, token: str = Heade
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid token")
 
     user_id = decoded_token["userId"]
-    identity_provider_access_token = identity_provider_jwt_encoder.generate_jwt({"exp":(datetime.now() + timedelta(seconds=5)).timestamp()})
+    identity_provider_access_token = identity_provider_jwt_encoder.generate_jwt({"exp":(datetime.now() + timedelta(minutes=1)).timestamp()})
     
     headers = {'Content-Type': 'application/json', 'userId':user_id, 'microserviceAccessToken':identity_provider_access_token}
     delete_user_response = requests.delete(f"https://cs-identity-provider.deta.dev/users", json=passwordIn.json(), headers=headers)
