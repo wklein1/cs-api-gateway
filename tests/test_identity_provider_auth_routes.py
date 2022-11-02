d8a084028725ddc96d9cb97a27d13bb6abb82052
wklein1 <Witali@>
2022-11-02 23:37:33 +0100

    #10 add login endpoint tests

---
+++ tests/test_identity_provider_auth_routes.py
@@ -0,0 +1,33 @@
+from fastapi.testclient import TestClient
+from fastapi import status
+from decouple import config
+from modules.jwt.jwt_module import JwtEncoder
+from main import app
+
+def test_login_user_endpoint_success():
+    #ARRANGE
+    client = TestClient(app)
+    test_user = {
+        "user_name":"test_usr",
+        "password":"testtesttest4"
+    }
+    #ACT
+    response = client.post("/login",json=test_user)
+    #ASSERT
+    assert response.status_code == 200
+    assert "token" in response.json()
+    assert response.json()["userName"] == "test_usr"
+
+
+def test_login_user_endpoint_fails_invalid_credentials():
+    #ARRANGE
+    client = TestClient(app)
+    test_user = {
+        "user_name":"test_usr",
+        "password":"testtesttest1"
+    }
+    #ACT
+    response = client.post("/login",json=test_user)
+    #ASSERT
+    assert response.status_code == 422
+    assert response.json() == {'detail': 'Unprocessable Entity'}
