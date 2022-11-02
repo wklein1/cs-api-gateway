4046e96711fa62bc7a8147c5f52a0b0f09e07f0b
wklein1 <Witali@>
2022-11-02 14:54:22 +0100

    #1 update get components endpoint test

--- tests/test_component_service_routes.py
+++ tests/test_component_service_routes.py
@@ -6,15 +6,15 @@ def test_get_components_endpoint_returns_components():
     response = client.get("/components")
     assert response.status_code == 200
     assert response.json()[0] == {
-        "id": 0,
-        "name": "EVGA SuperNOVA GT 850 850W",
+        "id": "546c08d7-539d-11ed-a980-cd9f67f7363d",
+        "name": "AMD Ryzen 9 5950X",
         "vendor": "notebooksbilliger.de",
-        "price": 89.90,
+        "price": 559.0,
         "description": "",
         "location": "Germany",
-        "manufacturer": "220-GT-0850-Y2",
-        "productGroup": "Power-supply",
+        "manufacturer": "100-100000059WOF",
+        "productGroup": "CPU",
         "weight": 300.0,
         "status": "new",
-        "eanNumber": "4250812439109"
+        "eanNumber": "730143312745"
   }
