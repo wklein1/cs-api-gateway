7d317ccffdd571f83904f2e12f477d4806d7cfc4
wklein1 <Witali@>
2022-11-03 18:43:58 +0100

    #15 add change password endpoint, fix tests

--- tests/tests_identity_provider_routes/test_identity_provider_handle_user_data_routes.py
+++ tests/tests_identity_provider_routes/test_identity_provider_handle_user_data_routes.py
@@ -105,7 +105,7 @@ def test_get_user_endpoint_fails_unauthorized_user_id():
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
     assert response.json() == expected_error
     #CLEANUP
-    client.delete("/users", json={"password":"testtesttest4"}, headers=auth_header)
+    client.delete("/users", json={"password":"testtesttest4"}, headers={"token":new_user["token"]})
 
 
 def test_change_password_endpoint_success():
@@ -126,14 +126,11 @@ def test_change_password_endpoint_success():
         "password":"testtesttest4",
         "new_password":"testtesttest5"
     }
-    auth_header = {
-          "token": new_user.json()["token"]
-    }
     new_user = client.post("/users",json=test_user)
     new_user = new_user.json()
     new_user_id = jwt_encoder.decode_jwt(new_user["token"],audience=jwt_aud,issuer=jwt_iss)["userId"]
     auth_header = {
-          "token": new_user.json()["token"]
+          "token": new_user["token"]
     }
     #ACT
     response = client.patch(f"/users/{new_user_id}/password", json=test_user_password_update, headers=auth_header)
@@ -168,7 +165,7 @@ def test_change_password_endpoint_fails_forbidden_wrong_password():
     new_user = new_user.json()
     new_user_id = jwt_encoder.decode_jwt(new_user["token"],audience=jwt_aud,issuer=jwt_iss)["userId"]
     auth_header = {
-          "token": new_user.json()["token"]
+          "token": new_user["token"]
     }
     #ACT
     response = client.patch(f"/users/{new_user_id}/password", json=test_user_password_update, headers=auth_header)
@@ -194,7 +191,7 @@ def test_change_password_endpoint_fails_invalid_token():
         "password":"testtesttest4"
     }
     test_user_password_update = {
-        "password":"invalid_password",
+        "password":"testtesttest4",
         "new_password":"testtesttest5"
     }
     expected_error = {
@@ -212,8 +209,7 @@ def test_change_password_endpoint_fails_invalid_token():
     assert response.status_code == 403
     assert response.json() == expected_error
     #CLEANUP
-    client.delete("/users", json={"password":"testtesttest4"}, headers=auth_header)
-
+    client.delete("/users", json={"password":"testtesttest4"}, headers={"token":new_user["token"]})
 
 
 def test_change_password_endpoint_fails_invalid_new_password():
@@ -238,7 +234,7 @@ def test_change_password_endpoint_fails_invalid_new_password():
     new_user = new_user.json()
     new_user_id = jwt_encoder.decode_jwt(new_user["token"],audience=jwt_aud,issuer=jwt_iss)["userId"]
     auth_header = {
-          "token": new_user.json()["token"]
+          "token": new_user["token"]
     }
     #ACT
     response = client.patch(f"/users/{new_user_id}/password", json=test_user_password_update, headers=auth_header)
@@ -251,7 +247,17 @@ def test_change_password_endpoint_fails_invalid_new_password():
 def test_change_password_endpoint_fails_user_not_found():
     #ARRANGE
     client = TestClient(app)
-
+    JWT_SECRET = config("JWT_SECRET")
+    jwt_aud="kbe-aw2022-frontend.netlify.app"
+    jwt_iss="cs-identity-provider.deta.dev"
+    jwt_encoder = JwtEncoder(JWT_SECRET, "HS256")
+    test_user = {
+        "first_name":"test",
+        "last_name":"test",
+        "user_name":"test_usr2",
+        "email":"test@test.com",
+        "password":"testtesttest4"
+    }
     test_user_password_update = {
         "password":"testtesttest4",
         "new_password":"testtesttest4"
@@ -259,8 +265,15 @@ def test_change_password_endpoint_fails_user_not_found():
     expected_error = {
         "detail": "User not found"
     }
+    new_user = client.post("/users",json=test_user)
+    new_user = new_user.json()
+    new_user_id = jwt_encoder.decode_jwt(new_user["token"],audience=jwt_aud,issuer=jwt_iss)["userId"]
+    auth_header = {
+          "token": new_user["token"]
+    }
+    client.delete("/users", json={"password":"testtesttest4"}, headers=auth_header)
     #ACT
-    response = client.patch("/users/invalid_user_id/password", json=test_user_password_update, headers=auth_header)
+    response = client.patch(f"/users/{new_user_id}/password", json=test_user_password_update, headers=auth_header)
     #ASSERT
     assert response.status_code == 404
     assert response.json() == expected_error
@@ -293,9 +306,9 @@ def test_delete_user_endpoint_success():
         "email":"test@test.com",
         "password":"testtesttest4"
     }
-    new_user = client.post("/users",json=test_user)
+    new_user = client.post("/users",json=test_user).json()
     auth_header = {
-          "token": new_user.json()["token"]
+          "token": new_user["token"]
     }
     #ACT
     response = client.delete("/users", json={"password":"testtesttest4"}, headers=auth_header)
