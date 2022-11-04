33be03ac12b868c678b4e5e2703b22f05881fc23
wklein1 <Witali@>
2022-11-04 16:17:59 +0100

    #23 add delete product endpoint tests

--- tests/tests_product_service_routes/test_product_service_routes.py
+++ tests/tests_product_service_routes/test_product_service_routes.py
@@ -72,3 +72,61 @@ def test_post_products_endpoint_fails_by_creating_not_owned_product():
     #ASSERT
     assert response.status_code == 403
     assert response.json() == expected_error
+
+
+def test_delete_product_endpoint_success():
+    #ARRANGE
+    client = TestClient(app)
+    TEST_USER_ID = config("TEST_USER_ID")
+    VALID_TOKEN = config("VALID_TOKEN")
+    test_product = {
+        "ownerId":TEST_USER_ID,
+        "name":"test new product",
+        "componentIds":["546c08d7-539d-11ed-a980-cd9f67f7363d","546c08da-539d-11ed-a980-cd9f67f7363d"],
+        "description":"new product from post request",
+        "price":0.0
+    }
+    auth_header = {
+          "token": VALID_TOKEN
+    }
+    post_response = client.post("/products",json=test_product, headers=auth_header)
+    product_id = post_response.json()["productId"]
+    #ACT
+    del_response = client.delete(f"/products/{product_id}",headers=auth_header)
+    #ASSERT
+    assert del_response.status_code == 204
+
+
+def test_delete_product_endpoint_fails_invalid_token():
+    #ARRANGE
+    client = TestClient(app)
+    TEST_USER_ID = config("TEST_USER_ID")
+    auth_header = {
+          "token": "invalid_token"
+    }
+    expected_error = {
+        "detail": "Invalid token"
+    }
+    #ACT
+    del_response = client.delete("/products/some_product_id",headers=auth_header)
+    #ASSERT
+    assert del_response.status_code == 403
+    assert del_response.json() == expected_error
+
+
+def test_delete_product_endpoint_fails_for_not_owned_product():
+    #ARRANGE
+    client = TestClient(app)
+    TEST_USER_ID = config("TEST_USER_ID")
+    VALID_TOKEN = config("VALID_TOKEN")
+    auth_header = {
+          "token": VALID_TOKEN
+    }
+    expected_error = {
+        "detail": "User is not allowed to delete a product not owned."
+    }
+    #ACT
+    del_response = client.delete(f"/products/######",headers=auth_header)
+    #ASSERT
+    assert del_response.status_code == 403
+    assert del_response.json() == expected_error
